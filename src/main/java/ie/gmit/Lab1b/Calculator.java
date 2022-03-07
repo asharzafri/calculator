65d3e4449704010f8be1442f47e5ee0537941979
asharzafri <72197044+asharzafri@users.noreply.github.com>
2022-03-07 15:33:43 +0000

    First add

---
+++ src/main/java/ie/gmit/Lab1b/Calculator.java
@@ -0,0 +1,16 @@
+package ie.gmit.Lab1b;
+
+public class Calculator {
+    int num1, num2;
+
+    public Calculator(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int add(){
+        int total = num1+ num2;
+
+        return total;
+    }
+}
