fe61c79f30e2b055e52cbadf6be16f359d610232
asharzafri <72197044+asharzafri@users.noreply.github.com>
2022-03-07 20:30:36 +0000

    Added new testcase for subtract

---
+++ src/test/java/ie/gmit/text/CalculatorTest.java
@@ -0,0 +1,18 @@
+package ie.gmit.text;
+
+import ie.gmit.Lab1b.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+        //Calculator calc = new Calculator();
+
+        @Test
+        void justAnExample() {
+        }
+        @Test
+        void testSubtract() {
+        }
+
+
+
+}
